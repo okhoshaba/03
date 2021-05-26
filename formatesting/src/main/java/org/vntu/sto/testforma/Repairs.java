7c7375b51f7bb07cecbd78c7471e7bd64259ac0f
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2021-05-26 18:56:27 +0300

    add all classes for IExtendedService

---
+++ formatesting/src/main/java/org/vntu/sto/testforma/Repairs.java
@@ -0,0 +1,14 @@
+package org.vntu.sto.testforma;
+
+class Repairs extends Straightening {
+  private String workCost;
+
+  public String getWorkCost() {
+    return " WorkCost: " + workCost;
+  }
+
+  public void setWorkCost(String workCost) {
+    this.workCost = workCost;
+  }
+}
+
