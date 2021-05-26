59fc5a417967c8161cc12cdd9ac9ec649a457cdf
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2021-05-26 18:50:22 +0300

    add some classes

---
+++ formatesting/src/main/java/org/vntu/sto/testforma/IExtendedService.java
@@ -0,0 +1,7 @@
+package org.vntu.sto.testforma;
+
+interface IExtendedService extends IService {
+  public String getClientName(); 
+  public void setClientName(String clientName); 
+}
+
---
+++ formatesting/src/main/java/org/vntu/sto/testforma/Shop.java
@@ -0,0 +1,14 @@
+package org.vntu.sto.testforma;
+
+class Shop extends Straightening {
+  private String productName;
+
+  public String getProductName() {
+    return " ProductName: " + productName;
+  }
+
+  public void setProductName(String productName) {
+    this.productName = productName;
+  }
+}
+
